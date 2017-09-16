d2795f49a9dcd19d5a7e5b640714f7241bcd19dd
Jason Ward <jason.ward@policystat.com>
2017-09-16 11:52:12 -0400

    refs #2: Super simple test to create a query string.

--- graphql_query_string_builder/tests/test_query_string_builder.py
+++ graphql_query_string_builder/tests/test_query_string_builder.py
@@ -1,6 +1,9 @@
 from unittest import TestCase
 
+from graphql_query_string_builder import Query
+
 
 class QueryStringBuilderTestCase(TestCase):
-    def test(self):
-        pass
+    def test_query_string_is_built(self):
+        query = Query(foo=['id', 'bar'])
+        self.assertEqual(str(query), 'query { foo { id bar }}')
