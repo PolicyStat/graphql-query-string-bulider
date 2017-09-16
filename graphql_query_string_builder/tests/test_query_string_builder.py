961b96a4249cf2216c7fab3e7f0c4fb5691681c5
Jason Ward <jason.ward@policystat.com>
2017-09-16 12:21:41 -0400

    refs #2: Added simple tests showing customization of the resulting query can be done.

--- graphql_query_string_builder/tests/test_query_string_builder.py
+++ graphql_query_string_builder/tests/test_query_string_builder.py
@@ -5,5 +5,22 @@ from graphql_query_string_builder import Query
 
 class QueryStringBuilderTestCase(TestCase):
     def test_query_string_is_built(self):
-        query = Query(foo=['id', 'bar'])
-        self.assertEqual(str(query), 'query { foo { id bar }}')
+        query = Query()
+        self.assertEqual(
+            query.render_query_string(foo=['id', 'bar']),
+            'query { foo { id bar }}',
+        )
+
+    def test_camel_case_happens_by_default(self):
+        query = Query()
+        self.assertEqual(
+            query.render_query_string(foo_bar_baz=['id', 'bar']),
+            'query { fooBarBaz { id bar }}',
+        )
+
+    def test_disabling_camel_case_removes_camel_case(self):
+        query = Query(auto_camel_case=False)
+        self.assertEqual(
+            query.render_query_string(foo_bar_baz=['id', 'bar']),
+            'query { foo_bar_baz { id bar }}',
+        )
